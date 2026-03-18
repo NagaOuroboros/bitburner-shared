d7eceb49a98beb70228c74cbad5d504b64a1da25
NagaOuroboros <tlath@hotmail.com>
2026-03-17 21:50:42 -0400

    initial commit

---
+++ option.ts
@@ -0,0 +1,133 @@
+/** A type the represents the possible absence of a value */
+export type Option<T> = Some<T> | None;
+
+export namespace Option {
+  /**
+   * Wraps the provided `value` in an instance of `Some`
+   * @param value The value to be wrapped
+   */
+  export function some<T>(value: T): Some<T> {
+    return new Some<T>(value);
+  }
+  /**
+   * Returns a `None` instance
+   */
+  export function none(): None {
+    return None.getInstance();
+  }
+  /**
+   * Takes a value that could potentially be `null` and wraps it in an `Option`
+   * @param value The value to be wrapped
+   */
+  export function fromNullable<T>(value: T | null | undefined): Option<T> {
+    return value == null ? none() : some(value);
+  }
+}
+
+export const some = Option.some;
+export const none = Option.none;
+export const fromNullable = Option.fromNullable;
+
+interface IOption<T> {
+  /** A Type Narrowing function that returns `true` if the underlying value is an instance of `Some` */
+  isSome(): this is Some<T>;
+  /** A Type Narrowing function that returns `true` if the underlying value is an instance of `None` */
+  isNone(): this is None;
+  /** 
+   * Returns the underlying `Some` value if it exists. Prefer `match` or `unwrapOr` for safer handling
+   * @throws Throws an `Error` if this is called on an instance of `None`
+   */
+  unwrap(): T;
+  /**
+   * Returns the underlying `Some` value if it exists, or the `fallback` value if it doesn't
+   * @param fallback The fallback to use if there is no underlying value
+   */
+  unwrapOr<F>(fallback: F): T | F;
+  /**
+   * Applies the provided function to the underlying `Some` value if it exists,
+   * and does nothing if the underlying value is an instance of `None`
+   * @param fn The function to apply to the underlying value
+   */
+  map<U>(fn: (value: T) => U): Option<U>;
+  /**
+   * Allows for chaining another function that returns an `Option` to the underlying `Some` value,
+   * and does nothing if the underlying value is an instance of `None`
+   * @param fn The function to apply to the underlying value. Must return an `Option`
+   */
+  andThen<U>(fn: (value: T) => Option<U>): Option<U>;
+  /**
+   * Returns the underlying `Option` if it is an instance of `Some`,
+   * or returns the `fallback` if it is an instance of `None`
+   * @param fallback An `Option` to use if the underlying value is not an instance of `Some`
+   */
+  orElse(fallback: Option<T>): Option<T>;
+  /**
+   * Conditionally applies a function that will resolve the `Option` to a final value
+   * @param onSome The function to apply if the underlying value is an instance of `Some`
+   * @param onNone The function to apply if the underlying value is an instance of `None`
+   */
+  match<A>(onSome: (value: T) => A, onNone: () => A): A;
+}
+
+class Some<T> implements IOption<T> {
+  readonly value: T;
+  constructor(value: T) {
+    this.value = value;
+  }
+  isSome(): this is Some<T> {
+    return true;
+  }
+  isNone(): this is None {
+    return false;
+  }
+  unwrap(): T {
+    return this.value;
+  }
+  unwrapOr<F>(_fallback: F): T {
+    return this.value;
+  }
+  map<U>(fn: (value: T) => U): Option<U> {
+    return new Some(fn(this.value));
+  }
+  andThen<U>(fn: (value: T) => Option<U>): Option<U> {
+    return fn(this.value);
+  }
+  orElse(_fallback: Option<T>): Option<T> {
+    return this;
+  }
+  match<A>(onSome: (value: T) => A, _onNone: () => A): A {
+    return onSome(this.value);
+  }
+}
+
+class None implements IOption<never> {
+  private static _instance = new None();
+  private constructor(){}
+  public static getInstance(): None {
+    return this._instance;
+  }
+  isSome(): this is Some<never> {
+    return false;
+  }
+  isNone(): this is None {
+    return true;
+  }
+  unwrap(): never {
+    throw new Error('Attempted to call unwrap on an instance of None.');
+  }
+  unwrapOr<F>(fallback: F): F {
+    return fallback;
+  }
+  map<U>(_fn: (value: never) => U): Option<U> {
+    return None.getInstance();
+  }
+  andThen<U>(_fn: (value: never) => Option<U>): Option<U> {
+    return None.getInstance();
+  }
+  orElse<T>(fallback: Option<T>): Option<T> {
+    return fallback;
+  }
+  match<A>(_onSome: (value: never) => A, onNone: () => A): A {
+    return onNone();
+  }
+}
---
+++ result.ts
@@ -0,0 +1,174 @@
+/** A type that represents the possibility of a success or an error */
+export type Result<T,E> = Ok<T, E> | Err<T, E>;
+
+export namespace Result {
+  /** 
+   * Wraps a value in an `Ok` instance
+   * @param value The value to be wrapped
+   */
+  export function ok<T>(value: T): Ok<T, never> {
+    return new Ok<T, never>(value);
+  }
+  /** 
+   * Wraps an error value in an `Err` instance
+   * @param error The error value to be wrapped
+   */
+  export function err<E>(error: E): Err<never, E> {
+    return new Err<never, E>(error);
+  }
+  /**
+   * Takes a synchronous function that could throw an Error and wraps the output in a `Result` type
+   * @param fn The function whose output is to be wrapped
+   * @param ctx Optional - A description of the context of the operation
+   */
+  export function fromThrowable<T>(fn: () => T, ctx?: string): Result<T, Error> {
+    try {
+      const res = fn();
+      return ok(res);
+    } catch (e) {
+      const error = e instanceof Error ? e : new Error(`${e}`);
+      if (ctx) {
+        error.message = `${ctx}: ${error.message}`;
+      }
+      return err(error);
+    }
+  }
+  /**
+   * Takes a `Promise` or async function that could throw an Error and wraps the output in a `Result` type
+   * @param fn The `Promise` or function whose output is to be wrapped
+   * @param ctx Optional - A description of the context of the operation
+   */
+  export async function fromThrowableAsync<T>(fn: () => Promise<T>, ctx?: string): Promise<Result<T, Error>> {
+    try {
+      const res = await fn();
+      return ok(res);
+    } catch (e) {
+      const error = e instanceof Error ? e : new Error(`${e}`);
+      if (ctx) {
+        error.message = `${ctx}: ${error.message}`;
+      }
+      return err(error);
+    }
+  }
+}
+
+export const ok = Result.ok;
+export const err = Result.err;
+export const fromThrowable = Result.fromThrowable;
+export const fromThrowableAsync = Result.fromThrowableAsync;
+
+interface IResult<T,E> {
+  /** A Type Narrowing function that returns `true` if the underlying value is an instance of `Ok` */
+  isOk(): this is Ok<T, E>;
+  /** A Type Narrowing function that returns `true` if the underlying value is an instance of `Err` */
+  isErr(): this is Err<T, E>;
+  /**
+   * Returns the underlying value if it is an instance of `Ok`. Prefer `match` or `unwrapOr` for safer handling
+   * @throws Throws an `Error` if called on an instance of `Err`
+   */
+  unwrap(): T;
+  /**
+   * Returns the underlying value if it is an instance of `Ok`,
+   * and returns the `fallback` value otherwise.
+   * @param fallback The value to use if the underlying value is not an instance of `Ok`
+   */
+  unwrapOr<F>(fallback: F): T | F;
+  /**
+   * Applies a function to the underlying `Ok` value and returns a new `Result`.
+   * Simply returns the existing `Result` in the case the underlying value is an instance of `Err`
+   * @param fn The function to apply the the underlying `Ok` value
+   */
+  map<U>(fn: (value: T) => U): Result<U, E>;
+  /**
+   * Applies a function to the underlying `Err` value and returns a new `Result`.
+   * Simply returns the existing `Result` in the case the underlying value is an instance of `Ok`
+   * @param fn The function to apply the the underlying `Err` value
+   */
+  mapErr<F>(fn: (error: E) => F): Result<T, F>;
+  /**
+   * Allows to chain another function that returns a `Result` on the underlying `Ok` value.
+   * Simply returns the existing `Result` in the case the underlying value is an instance of `Err`
+   * @param fn The function to apply the the underlying `Ok` value.  Must return a `Result`
+   */
+  andThen<U, F>(fn: (value: T) => Result<U, F>): Result<U, E | F>;
+  /**
+   * Allows to chain another function that returns a `Result` on the underlying `Err` value.
+   * Simply returns the existing `Result` in the case the underlying value is an instance of `Ok`
+   * @param fn The function to apply the the underlying `Err` value.  Must return a `Result`
+   */
+  orElse<U, F>(fn: (error: E) => Result<U, F>): Result<T | U, F>;
+  /**
+   * Conditionally resolves a `Result` by applying one of two functions to produce a final value
+   * @param onOk The function to apply if the underlying value is an `Ok` instance
+   * @param onErr The function to apply if the underlying value is an `Err` instance
+   */
+  match<A>(onOk: (value: T) => A, onErr: (error: E) => A): A;
+}
+
+class Ok<T, E> implements IResult<T, E> {
+  readonly value: T;
+  constructor(value: T) {
+    this.value = value;
+  }
+  isOk(): this is Ok<T, E> {
+    return true;
+  }
+  isErr(): this is Err<T,E> {
+    return false;
+  }
+  unwrap(): T {
+    return this.value;
+  }
+  unwrapOr<F>(_fallback: F): T {
+    return this.value;
+  }
+  map<U>(fn: (value: T) => U): Result<U, E> {
+    return new Ok(fn(this.value));
+  }
+  mapErr<F>(_fn: (error: E) => F): Result<T, F> {
+    return this as unknown as Result<T, F>;
+  }
+  andThen<U, F>(fn: (value: T) => Result<U, F>): Result<U, F> {
+    return fn(this.value);
+  }
+  orElse<U, F>(_fn: (value: E) => Result<U, F>): Result<T, F> {
+    return this as unknown as Result<T, F>
+  }
+  match<A>(onOk: (value: T) => A, _onErr: (error: E) => A): A {
+    return onOk(this.value);
+  }
+}
+
+class Err<T, E> implements IResult<T, E> {
+  readonly error: E;
+  constructor(error: E) {
+    this.error = error;
+  }
+  isOk(): this is Ok<T, E> {
+    return false;
+  }
+  isErr(): this is Err<T,E> {
+    return true;
+  }
+  unwrap(): T {
+    throw new Error('Attempted to call unwrap on an instance of Err.');
+  }
+  unwrapOr<F>(fallback: F): F {
+    return fallback;
+  }
+  map<U>(_fn: (value: T) => U): Result<U, E> {
+    return this as unknown as Result<U, E>;
+  }
+  mapErr<F>(fn: (error: E) => F): Result<T, F> {
+    return new Err(fn(this.error));
+  }
+  andThen<U, F>(_fn: (value: T) => Result<U, F>): Result<U, E> {
+    return this as unknown as Result<U, E>;
+  }
+  orElse<U, F>(fn: (error: E) => Result<U, F>): Result<U, F> {
+    return fn(this.error);
+  }
+  match<A>(_onOk: (value: T) => A, onErr: (error: E) => A): A {
+    return onErr(this.error);
+  }
+}
